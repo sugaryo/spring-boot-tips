7576dabc501ef46d3dac3ec3def218b1668c70d8
sugaryo <ryo.sugawara.1224@gmail.com>
2019-11-20 21:38:02 +0900

    first commit

---
+++ spring-boot-tips/src/main/java/sugaryo/springboot/tips/SpringBootTipsApplication.java
@@ -0,0 +1,13 @@
+package sugaryo.springboot.tips;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTipsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTipsApplication.class, args);
+	}
+
+}
---
+++ spring-boot-tips/src/test/java/sugaryo/springboot/tips/SpringBootTipsApplicationTests.java
@@ -0,0 +1,13 @@
+package sugaryo.springboot.tips;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTipsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
